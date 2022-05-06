e567e3bf1265ad3968e79fdadf63d73546902cc2
Alpha Chen <alpha@kejadlen.dev>
2022-05-06 11:51:15 -0700

    use latest alphred version

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use std::env;
 
-use alphred::Item;
+use alphred::{Item, Workflow};
+use anyhow::Result;
 use regex::Regex;
 use reqwest::blocking::Client;
-use serde_json::json;
 
 fn main() {
     let args: Vec<_> = env::args().skip(1).collect();
     let query = args.join(" ");
 
+    let html_re = Regex::new(r"(?i)<.*?>").unwrap();
+
+    println!(
+        "{}",
+        Workflow::new(|| Ok(frink(&query)?
+            .iter()
+            .map(|x| html_re.replace_all(x, ""))
+            .map(Item::new)
+            .collect()))
+    );
+}
+
+fn frink(query: &str) -> Result<Vec<String>> {
     let client = Client::new();
     let resp = client
         .get("https://frinklang.org/fsp/frink.fsp")
@@ -18,15 +31,9 @@ fn main() {
     let body = resp.text().unwrap();
 
     let results_re = Regex::new(r"(?is)<a name=results>(.*?)</a>").unwrap();
-    let html_re = Regex::new(r"(?i)<.*?>").unwrap();
 
     let cap = results_re.captures(&body).unwrap();
-
     let result = cap.get(1).unwrap().as_str();
-    let items: Vec<_> = result
-        .lines()
-        .map(|x| html_re.replace_all(x, ""))
-        .map(Item::new)
-        .collect();
-    println!("{}", json!({ "items": items }));
+
+    Ok(result.lines().map(str::to_string).collect())
 }
